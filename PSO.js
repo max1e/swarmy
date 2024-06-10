57aff4f8f1809151b548dad7d5815a6effc5c433
RikJansenn <50616888+RikJansenn@users.noreply.github.com>
2024-06-10 14:11:28 +0200

    Add files via upload

--- PSO.js
+++ PSO.js
@@ -4,9 +4,10 @@ let Scene = {
     walls : [], 
     swarm : [], 
     N : 200, 
-    target : [590, 300], 
+    target : [300, 590], 
     globalBest : (0,0), 
-    gbest_obj : Infinity
+    gbest_obj : Infinity,
+    distance : Array.from({ length: 600 }, () => Array(600).fill(Infinity))
 }
 
 const fat = 1
@@ -96,7 +97,7 @@ class Particle {
         let closestWall = createVector(0, 0)
         for (let wall of Scene.walls) {
             const closestPoint = this.closestPointOnLine(wall.A, wall.B, this.position)
-            if (dist(this.position, closestPoint) < dist(this.position, closestWall)) {
+            if (dist(this.position.x, this.position.y, closestPoint.x, closestPoint.y) < dist(this.position.x, this.position.y, closestWall.x, closestWall.y)) {
                 closestWall = closestPoint
             }
         }
@@ -218,12 +219,34 @@ function canUpdateY(new_y, current_pos) {
 
 // Objective function: returns score based on position
 function objective_function(x, y){
-    return dist( x, y, Scene.target[0], Scene.target[1] )
+    //return dist( x, y, Scene.target[0], Scene.target[1] )
+    return Scene.distance[Math.round(x)][Math.round(y)]
 }
 
-function setup(){
-	createCanvas( Scene.width, Scene.height )
+function getDistances(){
+    Scene.distance[Scene.target[0]][Scene.target[1]] = 0
+    
+    const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+  
+    const queue = [[Scene.target[0], Scene.target[1]]]
+    
+    while(queue.length > 0) {
+        const [x, y] =  queue.shift()
+        for (const [dx, dy] of directions){
+            const nx = x + dx
+            const ny = y + dy
+            
+            if (nx >= 0 && nx < Scene.width && ny >= 0 && ny < Scene.height && Scene.distance[nx][ny] != 6666) {
+                if (Scene.distance[nx][ny] > Scene.distance[x][y] + 1) {
+                    Scene.distance[nx][ny] = Scene.distance[x][y] + 1
+                    queue.push([nx, ny])
+                }
+            }
+        }
+    }
+}
 
+function createWalls(){
     Scene.walls = [
         new Wall(100, 100, 500, 100),
         new Wall(100, 100, 100, 500),
@@ -231,8 +254,25 @@ function setup(){
         new Wall(500, 100, 500, 275),
         new Wall(500, 325, 500, 500)
     ]
+  
+    for (let wall of Scene.walls) {
+        for (let x = wall.A.x ; x <=wall.B.x ; x++) {
+            for (let y = wall.A.y ; y <=wall.B.y ; y++) {
+                Scene.distance[x][y] = 6666 //idk
+            }
+        }
+    }
+}
 
+function setup(){
+	createCanvas( Scene.width, Scene.height )
+    createWalls()  
+  
+    getDistances()
+    print(Scene.distance)
+  
     Scene.swarm = []
+
 	for (let i = 0; i < Scene.N; i++){
         Scene.swarm.push(new Particle())
 	}
